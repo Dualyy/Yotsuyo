676b5c4291f57fae4c1ca12b25a3e172bd80fea6
Dualyy <l.fraessle@t-online.de>
2018-05-10 14:03:41 +0200

    wtf2

---
+++ Yotsuyo/src/main/java/dually/bot/Commands/PingCommand.java
@@ -0,0 +1,17 @@
+package dually.bot.Commands;
+
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class PingCommand extends ListenerAdapter {
+	
+	void onMessageRecieved(MessageReceivedEvent event) {
+		
+		MessageChannel channel = event.getChannel();
+		if(event.getMessage().getContentRaw().equalsIgnoreCase("!ping") && !event.getAuthor().isBot()) {
+			channel.sendMessage("pong");
+		}
+	}
+
+}
