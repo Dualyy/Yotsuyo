d781115a92954ac6003dbcbe030254ae0ed2f759
Dualyy <l.fraessle@t-online.de>
2018-05-10 14:17:56 +0200

    ffs

--- Yotsuyo/src/main/java/dually/bot/Commands/PingCommand.java
+++ Yotsuyo/src/main/java/dually/bot/Commands/PingCommand.java
@@ -9,7 +9,7 @@ public class PingCommand extends ListenerAdapter {
 	void onMessageRecieved(MessageReceivedEvent event) {
 		
 		MessageChannel channel = event.getChannel();
-		if(event.getMessage().getContentStripped().equalsIgnoreCase("!ping") && !event.getAuthor().isBot()) {
+		if(event.getMessage().getContentDisplay().equalsIgnoreCase("!ping") && !event.getAuthor().isBot()) {
 			channel.sendMessage("pong").queue();
 		}
 	}
