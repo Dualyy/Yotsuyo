a7f102b9e440df9d9a0a258d0550b6749902186e
Dualyy <l.fraessle@t-online.de>
2018-05-10 13:23:13 +0200

    bot

--- Yotsuyo/src/main/java/dually/bot/Main.java
+++ Yotsuyo/src/main/java/dually/bot/Main.java
@@ -1,9 +1,37 @@
 package dually.bot;
 
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("wtf is goin gon");
+		String token = args[0];
+		try
+        {
+            JDA jda = new JDABuilder(AccountType.BOT)
+                    .setToken(token)           //The token of the account that is logging in.
+                    .buildBlocking();  //There are 2 ways to login, blocking vs async. Blocking guarantees that JDA will be completely loaded.
+        }
+        catch (LoginException e)
+        {
+            //If anything goes wrong in terms of authentication, this is the exception that will represent it
+            e.printStackTrace();
+        }
+        catch (InterruptedException e)
+        {
+            //Due to the fact that buildBlocking is a blocking method, one which waits until JDA is fully loaded,
+            // the waiting can be interrupted. This is the exception that would fire in that situation.
+            //As a note: in this extremely simplified example this will never occur. In fact, this will never occur unless
+            // you use buildBlocking in a thread that has the possibility of being interrupted (async thread usage and interrupts)
+            e.printStackTrace();
+        }
 	}
 
 }
